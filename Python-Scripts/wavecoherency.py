501b85e7e76b2933900119138e4d0f183882b2b7
Willy Hagi <hagi.willy@gmail.com>
2018-07-14 15:03:30 -0400

    XCT and Coherence.
    A modified version of the Cross Wavelet sample script from the PyCWT package. 
    
    The original version can be found at https://github.com/regeirk/pycwt/blob/master/pycwt/sample/sample_xwt.py

---
+++ Python-Scripts/wavecoherency.py
@@ -0,0 +1,298 @@
+"""
+Python sample script for cross-wavelet analysis and the statistical approach
+suggested by Torrence and Compo (1998) using the wavelet module. To run
+this script successfully, the `matplotlib` and `progressbar` modules have to
+be installed.
+
+
+Disclaimer
+----------
+This module is based on routines provided by C. Torrence and G. P. Compo
+available at <http://paos.colorado.edu/research/wavelets/>, on routines
+provided by A. Grinsted, J. Moore and S. Jevrejeva available at
+<http://noc.ac.uk/using-science/crosswavelet-wavelet-coherence>, and
+on routines provided by A. Brazhe available at
+<http://cell.biophys.msu.ru/static/swan/>.
+
+This software is released under a BSD-style open source license. Please read
+the license file for furter information. This routine is provided as is
+without any express or implied warranties whatsoever.
+
+Authors
+-------
+Nabil Freij, Sebastian Krieger
+
+"""
+
+# Kindly modified by:
+# Willy Hagi (UEA/EST)
+# hagi.willy@gmail.com
+
+
+
+
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pycwt as wavelet
+
+from pycwt.helpers import find
+from matplotlib.image import NonUniformImage
+
+
+
+
+##--- input time series
+data1 = dict(name='Arctic Oscillation',
+        nick='AO', file='jao.txt')
+
+data2 = dict(name='Baltic Sea ice extent',
+        nick='BMI', file='jbaltic.txt')
+
+
+
+# Loads the data to be analysed.
+t1, s1 = np.loadtxt(data1['file'], unpack=True)
+t2, s2 = np.loadtxt(data2['file'], unpack=True)
+dt = np.diff(t1)[0]
+n1 = t1.size
+n2 = t2.size
+n = min(n1, n2)
+
+
+# Change the probablity density function (PDF) of the data. The time series
+# of Baltic Sea ice extent is highly bi-modal and we therefore transform the
+# timeseries into a series of percentiles. The transformed series probably
+# reacts 'more linearly' to climate.
+s2, _, _ = wavelet.helpers.boxpdf(s2)
+
+
+# Calculates the standard deviation of each time series for later
+# normalization.
+std1 = s1.std()
+std2 = s2.std()
+
+
+
+
+
+# I. Continuous wavelet transform
+# ===============================
+
+
+
+# Calculate the CWT of both normalized time series. The function wavelet.cwt
+# returns a a list with containing [wave, scales, freqs, coi, fft, fftfreqs]
+# variables.
+mother = wavelet.Morlet(6)          # Morlet mother wavelet with m=6
+slevel = 0.95                       # Significance level
+dj = 1/12                           # Twelve sub-octaves per octaves
+s0 =  2 * dt                   # Starting scale, here 6 months
+J = 6 / dj                    # Seven powers of two with dj sub-octaves
+if True:
+    alpha1, _, _ = wavelet.ar1(s1)  # Lag-1 autocorrelation for red noise
+    alpha2, _, _ = wavelet.ar1(s2)  # Lag-1 autocorrelation for red noise
+else:
+    alpha1 = alpha2 = 0.0           # Lag-1 autocorrelation for white noise
+
+
+
+# The following routines perform the wavelet transform and siginificance
+# analysis for two data sets.
+
+mother = 'morlet' 
+
+W1, scales1, freqs1, coi1, _, _ = wavelet.cwt(s1/std1, dt, dj, s0, J, mother)
+signif1, fft_theor1 = wavelet.significance(1.0, dt, scales1, 0, alpha1,
+                                           significance_level=slevel,
+                                           wavelet=mother)
+W2, scales2, freqs2, coi2, _, _ = wavelet.cwt(s2/std2, dt, dj, s0, J, mother)
+signif2, fft_theor2 = wavelet.significance(1.0, dt, scales2, 0, alpha2,
+                                           significance_level=slevel,
+                                           wavelet=mother)
+
+power1 = (np.abs(W1)) ** 2             # Normalized wavelet power spectrum
+power2 = (np.abs(W2)) ** 2             # Normalized wavelet power spectrum
+period1 = 1/freqs1
+period2 = 1/freqs2
+sig95_1 = np.ones([1, n1]) * signif1[:, None]
+sig95_1 = power1 / sig95_1             # Where ratio > 1, power is significant
+sig95_2 = np.ones([1, n2]) * signif2[:, None]
+sig95_2 = power2 / sig95_2             # Where ratio > 1, power is significant
+
+
+
+
+
+# II. Cross-wavelet transform
+# ===========================
+
+# Due to the difference in the time series, the second signal 
+# has to be trimmed for the XWT process.
+s2 = s2[np.argwhere((t2 >= min(t1)) & (t2 <= max(t1))).flatten()]
+
+''' Calculate the cross wavelet transform (XWT). 
+The XWT finds regions in time frequency space where the time series
+show high common power. Torrence and Compo (1998) state that the 
+percent point function -- PPF (inverse of the 
+cumulative distribution function) -- of a chi-square distribution 
+at 95% confidence and two degrees of freedom is Z2(95%)=3.999. 
+However, calculating the PPF using chi2.ppf gives Z2(95%)=5.991. 
+To ensure similar significance intervals as in Grinsted et al. (2004),
+one has to use confidence of 86.46%. '''
+W12, cross_coi, freq, signif = wavelet.xwt(s1, s2, dt,
+                                           dj=1/12, s0=-1, J=-1,
+                                           significance_level=0.8646,
+                                           wavelet='morlet',
+                                           normalize=True)
+
+cross_power = np.abs(W12)**2
+cross_sig = np.ones([1, n]) * signif[:, None]
+cross_sig = cross_power / cross_sig  # Power is significant where ratio > 1
+cross_period = 1/freq
+
+
+
+'''Calculate the wavelet coherence (WTC).
+The WTC finds regions in time frequency space where the
+two time seris co-vary, but do not necessarily have high power.'''
+WCT, aWCT, corr_coi, freq, sig = wavelet.wct(s1, s2, dt,
+                                             dj=1/12, s0=-1, J=-1,
+                                             significance_level=0.8646,
+                                             wavelet='morlet', normalize=True,
+                                             cache=True)
+
+cor_sig = np.ones([1, n]) * sig[:, None]
+cor_sig = np.abs(WCT) / cor_sig  # Power is significant where ratio > 1
+cor_period = 1 / freq
+
+
+
+''' Calculates the phase between both time series. 
+The phase arrows in the cross wavelet power spectrum rotate clockwise
+with 'north' origin.
+The relative phase relationship convention is the same as adopted
+by Torrence and Webster (1999), where in phase signals point
+upwards (N), anti-phase signals point downwards (S). If X leads Y,
+arrows point to the right (E) and if X lags Y, arrow points to the
+left (W). '''
+
+angle = 0.5 * np.pi - aWCT
+u, v = np.cos(angle), np.sin(angle)
+
+
+
+
+
+##----------------------- PLOTTING
+
+
+
+##--- Cross-Wavelet Spectrum and Phase 
+fig, (ax1) = plt.subplots(figsize=(12,7),nrows=1, ncols=1,
+                  sharex=True, sharey=True)
+
+extent_cross = [t1.min(), t1.max(), 0, max(cross_period)]
+
+
+
+# power spectrum contours
+im1 = ax1.contourf(t1, cross_period, cross_power, 60,
+            cmap='rainbow')
+
+# statistical significance contours
+ax1.contour(t1, cross_period, cross_sig,
+           [-99, 1], colors='k', linewidths=2,
+            extent=extent_cross)
+
+# (literal) cone of influence
+ax1.fill(np.concatenate([t1, t1[-1:]+dt, t1[-1:]+dt,
+                         t1[:1]-dt, t1[:1]-dt]),
+         np.concatenate([cross_coi, [1e-9], cross_period[-1:],
+                         cross_period[-1:], [1e-9]]),
+         color='Black',alpha=0.5,hatch="x")
+
+# phase arrows
+ax1.quiver(t1[::3], cross_period[::3], u[::3, ::3], v[::3, ::3],
+           units='width', angles='uv', pivot='mid', linewidth=1,
+           edgecolor='k', headwidth=10, headlength=10,
+           headaxislength=5,
+           minshaft=2, minlength=5)
+
+
+# set axis limits and range
+ax1.set_xlim([1855, 1990])
+ax1.set_xticks(range(1855,2000, 10))
+
+ax1.set_ylim([2,34])
+ax1.set_yticks(range(2,36, 4))
+
+
+ax1.invert_yaxis() # invert y axis (scales)
+
+plt.colorbar(im1, orientation='horizontal', shrink=0.5)
+plt.xlabel('Time (years)',fontsize=11)
+plt.ylabel('Period (years)',fontsize=11)
+plt.title('Cross-Wavelet Spectrum',fontsize=11)
+
+
+plt.tight_layout()
+plt.show()
+
+
+
+##--- Wavelet Coherence Spectrum and Phase 
+fig, (ax2) = plt.subplots(figsize=(12,7),nrows=1, ncols=1,
+                  sharex=True, sharey=True)
+
+extent_corr =  [t1.min(), t1.max(), 0, max(cor_period)]
+inter       =  np.arange(0, 1.1, 0.1)
+
+# coherence spectrum contours
+im2 =  ax2.contourf(t1, cor_period, WCT, inter,
+                    cmap='rainbow')
+
+# statistical significance contours
+ax2.contour(t1, cor_period, cor_sig, [-99, 1],
+            colors='k', linewidths=2,
+            extent=extent_corr)
+
+# (literal) cone of influence
+ax2.fill(np.concatenate([t1, t1[-1:]+dt, t1[-1:]+dt,
+                         t1[:1]-dt, t1[:1]-dt]),
+         np.concatenate([corr_coi, [1e-9], cor_period[-1:],
+                         cor_period[-1:],
+                         [1e-9]]),
+         'k', alpha=0.3, hatch='x')
+
+# phase arrows
+ax2.quiver(t1[::3], cor_period[::3], u[::3, ::3], v[::3, ::3],
+           units='height',
+           angles='uv', pivot='mid', linewidth=1, edgecolor='k',
+           headwidth=10, headlength=10, headaxislength=5, minshaft=2,
+           minlength=5)
+
+plt.colorbar(im2, orientation='horizontal', shrink=0.5)
+
+
+
+# set axis limits and range
+ax2.set_xlim([1855, 1990])
+ax2.set_xticks(range(1855,2000, 10))
+
+ax2.set_ylim([2,34])
+ax2.set_yticks(range(2,36, 4))
+
+ax2.invert_yaxis() # invert y axis
+
+
+plt.xlabel('Time (years)',fontsize=11)
+plt.ylabel('Period (years)',fontsize=11)
+plt.title('Wavelet Coherency Spectrum',fontsize=11)
+
+
+plt.tight_layout()
+plt.show()
