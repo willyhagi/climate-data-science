00b872554ec9a29365af199384c87c3b94cabae7
Willy Hagi <hagi.willy@gmail.com>
2019-12-12 16:47:47 -0400

    Update annual_cycle.py

--- Python-Scripts/annual_cycle.py
+++ Python-Scripts/annual_cycle.py
@@ -11,152 +11,52 @@
 '''
 
 
+import numpy as np
+import xarray as xr
+import proplot as plot
+import matplotlib.pyplot as plt
+
+from esmtools.stats import*
+from calendar import month_name
 
 
+##--- read netcdf file
+dset = xr.open_dataset('precip.mon.total.v2018.nc')
+#--- select an area and time (optional)
+dset = dset.sel(lat=slice(15, -60), lon=slice(270, 330))
 
-import matplotlib.pyplot   as plt
-import cartopy.crs         as ccrs
-import cartopy.feature     as cf
-import cartopy             as cartopy
-import numpy               as np
-import xarray              as xr
-import datetime
-
-from calendar              import month_name
-from functions             import* # from functions.py file
-from cartopy.mpl.ticker    import LongitudeFormatter, LatitudeFormatter
-from scipy.signal          import detrend
+##--- monthly climatology and anomalies with respect to the 1981-2010 period
+base_period = dset['precip'].sel(time=slice('1981-1-1', '2010-12-1'))
+wrt = base_period.groupby('time.month').mean('time')
+anomalies = dset['precip'].groupby('time.month') - wrt
 
 
+##--- detrend and normalize by the standard deviation
+anom_dt = rm_poly(anomalies, 1, dim='time')
+anom_norm = anom_dt / base_period.std('time')
+
+##--- save files (optional)
+#anom_dt.to_netcdf('asstdt.nc')
+#anom_norm.to_netcdf('asstdt_norm.nc')
 
 
+##--- plotting
+f, ax = plot.subplots(axwidth=1.5, nrows=3, ncols=4, tight=True,
+                      proj='pcarree', proj_kw={'lon_0': 0},)
 
-##----- read netcdf file
-dset  =  xr.open_dataset('precip.mon.total.1x1.v7.nc')
-var   =  dset['precip'][:,:,:]
-lat   =  dset['lat'][:]
-lon   =  dset['lon'][:]
+ax.format(land=False, coast=True, innerborders=True, borders=True,
+          large='15px', labels=True,
+          latlim=(16, -51), lonlim=(269, 329),
+          geogridlinewidth=0,
+          abcloc='ur',
+          )
 
-
-
-##----- if you want to select a certain area (and time) over the globe
-
-# south america
-lat1  =  12  
-lat2  =  -50  
-lon1  =  270  
-lon2  =  330  
-
-
-time1 = '1981-1-1' ; time2 = '2010-12-1'
-
-
-
-##----- numpy array converting (useful for plotting)
-lon  =  var.sel(lon=slice(lon1,lon2))  
-lon  =  np.asarray(lon.lon.values)
-
-lat  =  var.sel(lat=slice(lat1,lat2)) 
-lat  =  np.asarray(lat.lat.values)
-
-
-
-
-
-##----------------------- CALCULATIONS
-
-fclim  =  Demean(var, lat1, lat2, lon1, lon2, time1, time2)  # call Demean class
-clim   =  fclim.average()                                    # monthly mean
-anom   =  fclim.anomaly()                                    # anomalies
-index  =  fclim.area_avg()                                   # index calculation
-
-
-
-##----- DETRENDING (OPTIONAL)
-''' Note: it is highly advisable to detrend you data at this point. It can save you from some unnecessary 
-repetitions of lines of code later (when, for example, if you want to perform some EOF analysis), avoiding
-any kind of conflict between Xarray and SciPy AND making your things run faster. The following lines 
-are mostly from Dr. Nicholas Fauchereau's notebooks (http://nicolasfauchereau.github.io/climatecode/posts/).'''
-
-
-##--- processing of composite matrix
-anom = np.asarray(anom)
-naxis  =  [np.newaxis]
-
-# masking
-asst_nan   =  Nan_calc(anom, time, lat, lon)
-asst_res   =  asst_nan.reshaping()
-val, anom  =  asst_nan.masking_array()
-
-# detrend
-anom = detrend(anom, type='linear')
-
-# rec 
-anom   =  rec_matrix(anom, time, lat, lon, val) 
-
-
-d = {}
-d['time'] = ('time',time)
-d['lat'] = ('lat',lat)
-d['lon'] = ('lon', lon)
-d['sst'] = (['time','lat','lon'], anom)
-
-dset_from_dict = xr.Dataset(d)
-print (type(dset_from_dict))
-
-dset_from_dict.to_netcdf('asstv5.nc')
-
-
-
-
-#--- saving files
-#anom = anom.to_dataset()
-#anom.to_netcdf('aprp_teste.nc')            # save anomaly values to netcdf file
-#np.savetxt('index.asc',index,fmt='%.10f')  # save index to text file
-
-
-
-
-
-##----------------------- PLOTTING
-
-
-
-interval  = np.arange(0,700,100)
-proj      =  ccrs.PlateCarree()     # cartopy projection
-
-fig, axes = plt.subplots(figsize=(10,12),nrows = 3, 
-						 ncols = 4, subplot_kw={'projection': proj})
-
-axes = axes.flatten()
-
+# don't know why the month title is not working
 for i, month in enumerate(range(1,13,1)):
-    ax = axes[i]
-#    ax.set_extent([280, 325, -50, 12], proj)                 # select lon/lat
-    ax.add_feature(cf.BORDERS,zorder=2,edgecolor='k')        # borders
-    ax.add_feature(cartopy.feature.COASTLINE)                # coastline
-    x_lons = np.arange(-85,-15,20)
-    y_lats = np.arange(-70,50,10)
-    tick_fs = 16
-    ax.set_xticks(x_lons, minor=False, crs=proj)
-    ax.set_yticks(y_lats, minor=False, crs=proj)
-    lon_formatter = LongitudeFormatter(zero_direction_label=True,
-                                   number_format='.0f')
-    lat_formatter = LatitudeFormatter()
-    ax.xaxis.set_major_formatter(lon_formatter)
-    ax.yaxis.set_major_formatter(lat_formatter)
-    ax.set_title(month_name[i+1])  # month titles
-    data = ax.contourf(lon,lat,clim[i,:,:],interval,
-                       transform = proj,
-                       cmap = 'rainbow')
-    fig.colorbar(data, ax=ax, shrink=0.5)
+    data = ax.contourf(wrt['lon'], wrt['lat'], wrt[i,:,:],
+                       levels = np.arange(0, 550, 50),
+                       cmap = 'BuPu',
+                       extend='both')
+    ax.format(title='{:s}'.format(month_name[i+1]))
 
-plt.tight_layout()
-#plt.savefig('fig.eps',format='eps')  # save figure
 plt.show()
-
-
-
-
-
-# A. M. D. G. #
